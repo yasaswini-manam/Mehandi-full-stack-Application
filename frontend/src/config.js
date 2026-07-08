bfcb5dd2681032eeea51d00396799048bbf4f7b4
Manam Yasaswini <yasaswini_manam@srmap.edu.in>
2026-07-08 20:10:00 +0530

    fix

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL = 
   window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
     ? "http://localhost:8080"
-    : "https://mehendiapplication-production.up.railway.app";
+    : "https://distinguished-intuition-production-ced3.up.railway.app";
